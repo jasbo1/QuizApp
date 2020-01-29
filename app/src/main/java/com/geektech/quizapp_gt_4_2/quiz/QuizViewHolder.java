dbdce7ccc143ef019898cdceb2d80c38135237dc
jasbo1 <chocolateoops@gmail.com>
2020-01-29 18:28:28 +0300

    Added Quiz components Changed status bar color Changed seekbar color Questions amount meaning deleted bottom navigation text

---
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/quiz/QuizViewHolder.java
@@ -0,0 +1,12 @@
+package com.geektech.quizapp_gt_4_2.quiz;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class QuizViewHolder extends RecyclerView.ViewHolder {
+    public QuizViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
---
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/quiz/QuizViewModel.java
@@ -0,0 +1,6 @@
+package com.geektech.quizapp_gt_4_2.quiz;
+
+import androidx.lifecycle.ViewModel;
+
+public class QuizViewModel extends ViewModel {
+}
